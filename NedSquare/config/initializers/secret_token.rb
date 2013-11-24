d53ba810b1a0888b2b5ae2b96232b563b8b8f0ad
email <nednadima@bonaparteimpress.com>
2013-11-23 20:12:22 -0500

    initial commit

---
+++ NedSquare/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NedSquare::Application.config.secret_key_base = 'b2cca795d6724df811a64431c5b99a9079f517d89b45d26f4b8283d9ab71f2918654b138111c769aea7455b053c1f1c5fab1b3e53947cf887d506a5ff485c5ab'
